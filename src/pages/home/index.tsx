eb2af7c752ecdc668e31e859c8bdc4f2f526cc59
Alexandre Marquez de Moura Beicker <alexandremmbeicker@gmail.com>
2023-10-12 11:38:01 -0300

    adicionando css modules

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import styles from './home.module.css'
+import { Link } from 'react-router-dom'
 export function Home (){
     return(
-        <div>
+        <div className={styles.container}>
             <h1>Página Home</h1>
+        <Link to='/detail'>
+            <h1>Página de detalhes</h1>
+        </Link>
         </div>
     )
 }
